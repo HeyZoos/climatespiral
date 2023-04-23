88dc8e10235205b936bf8d4a5edfa86585a36dfd
Jesus Bracho <jessebracho@gmail.com>
2023-04-23 00:33:28 -0400

    fix: treat '***' as a null value

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     let df = CsvReader::from_path("data/giss.csv")
         .unwrap()
         .with_skip_rows(1)
+        .with_null_values(Some(NullValues::AllColumnsSingle("***".to_string())))
         .finish()
         .unwrap();
     dbg!(df);
