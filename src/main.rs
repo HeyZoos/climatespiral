14fcfe3bac4275c97e8d759f497648cde884d2d6
Jesus Bracho <jessebracho@gmail.com>
2023-04-23 00:44:16 -0400

    feat: add nannou boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use nannou::prelude::*;
 use polars::prelude::*;
 
 fn main() {
+    nannou::app(model).event(event).simple_window(view).run();
+}
+
+fn model(_app: &App) -> Model {
     let df = CsvReader::from_path("data/giss.csv")
         .unwrap()
         .with_skip_rows(1)
@@ -8,4 +13,12 @@ fn main() {
         .finish()
         .unwrap();
     dbg!(df);
+
+    Model {}
 }
+
+fn event(_app: &App, _model: &mut Model, _event: Event) {}
+
+fn view(_app: &App, _model: &Model, _frame: Frame) {}
+
+struct Model {}
