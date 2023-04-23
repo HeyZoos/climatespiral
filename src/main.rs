0f87d1b893961d661bc6a73cdc012064764424f6
Jesus Bracho <jessebracho@gmail.com>
2023-04-23 00:31:38 -0400

    fix: skip first row

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use polars::prelude::*;
 fn main() {
     let df = CsvReader::from_path("data/giss.csv")
         .unwrap()
+        .with_skip_rows(1)
         .finish()
         .unwrap();
     dbg!(df);
